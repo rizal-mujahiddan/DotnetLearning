415a30b1b23fb8665aa17fc59cfecff84dcda459
rizal mujahiddan <rizal.mujahiddan@gmail.com>
2026-01-01 21:49:17 +0700

    Explore service lifetimes

--- MyWebAppDI/Program.cs
+++ MyWebAppDI/Program.cs
@@ -3,10 +3,15 @@ using MyWebAppDI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<IWelcomeService, WelcomeService>();
+builder.Services.AddTransient<IWelcomeService, WelcomeService>();
 
 var app = builder.Build();
 
-app.MapGet("/", (IWelcomeService welcomeService) => welcomeService.GetWelcomeMessage());
+app.MapGet("/", async (IWelcomeService welcomeService1, IWelcomeService welcomeService2) =>
+{
+    string message1 = $"Message1: {welcomeService1.GetWelcomeMessage()}";
+    string message2 = $"Message1: {welcomeService2.GetWelcomeMessage()}";
+    return $"{message1}\n{message2}";
+});
 
 app.Run();
